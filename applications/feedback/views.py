ac84f0b13e17aa122a0013dadf9ccbd367a1eb1d
szhokee <szhokee@gmail.com>
2023-05-11 00:54:35 +0600

    feed

---
+++ applications/feedback/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins
+from applications.feedback.models import Favorite, Like, Rating, Comment
+from applications.feedback.serializers import FavoriteSerializer, LikeSerializer, RatingSerializer, CommentSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class LikeModelViewSet(ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+class RatingModelViewSet(ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+    
+
+class CommentModelViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+
+
+
+class FavoriteModelViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   GenericViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        return serializer.save(owner=self.request.user)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(owner=self.request.user)
+        return queryset
--- applications/films/urls.py
+++ applications/films/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-
 from rest_framework.routers import DefaultRouter
-
 from applications.films.views import FilmsViewSet
+
 
 router = DefaultRouter()
 router.register('', FilmsViewSet)
