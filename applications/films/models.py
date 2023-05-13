075dec8f53b02fc085e954a72ddaf7faed582d45
sora-yuka <feelingjeez@gmail.com>
2023-05-13 21:49:20 +0600

    changed media url

--- applications/films/models.py
+++ applications/films/models.py
@@ -15,9 +15,9 @@ class Genre(models.Model):
 class Films(models.Model):    
     title = models.CharField(max_length=100)
     genres = models.ManyToManyField(Genre, verbose_name="жанры")
-    avatar = models.ImageField(upload_to='films_avatar/')
-    film = models.FileField(upload_to='films_film/')
-    trailer = models.FileField(upload_to='film_trailer')
+    avatar = models.ImageField(upload_to='film/films_avatar/')
+    film = models.FileField(upload_to='film/films_film/')
+    trailer = models.FileField(upload_to='film/film_trailer')
     director = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     upload_at = models.DateTimeField(auto_now_add=True)
--- config/settings.py
+++ config/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
